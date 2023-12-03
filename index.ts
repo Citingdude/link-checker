90990695fb03fb58709a6470a05c79aa89090700
Glenn <glenn.reumers@appwise.be>
2023-12-03 10:24:19 +0100

    Refactor url-checker with functions

--- index.ts
+++ index.ts
@@ -2,42 +2,58 @@ import { parse } from 'node-html-parser';
 
 async function main() {    
     const baseURL = 'https://vibefusion.be'
-    const response = await fetch('https://vibefusion.be')
 
-    const invalidLinks = []
-
-    const html = await response.text()
-
-    const parsedHTML = parse(html)
-    const links = parsedHTML.getElementsByTagName('a')
+    const invalidLinks: string[] = []
     const urlsToCheck = new Set<string>()
 
-    links.forEach(link => {
-      const href = link.getAttribute('href')
-      const url = baseURL + href
+    const links = await fetchLinks('https://vibefusion.be', baseURL)
 
-      urlsToCheck.add(url)
-    });
+    setLinksToCheck(links, urlsToCheck)
 
-    for (const url of urlsToCheck) {
-      try {
-          const linkIsValid = await checkLink(url)
-
-          if (!linkIsValid)
-              invalidLinks.push(url)
-
-      } catch (error) {
-          invalidLinks.push(url)
-      }
-    }
+    checkLinks(urlsToCheck, invalidLinks)
     
     console.log(invalidLinks)
 }
 
+
+
+async function fetchLinks(url: string, baseURL: string) {
+  const response = await fetch(url)
+  const html = await response.text()
+  const parsedHTML = parse(html)
+  const links = parsedHTML.getElementsByTagName('a')
+
+  return links.map((link) => {
+    const href = link.getAttribute('href')
+    const url = baseURL + href
+    return url
+  })
+}
+
+function setLinksToCheck(urls: string[], urlSet: Set<string>) {
+  urls.forEach((url) => {
+    urlSet.add(url)
+  })    
+}
+
 async function checkLink(url: string) {
-    console.log('Checking url: ' + url)
-    const response = await fetch(url)
-    return response.status === 200
+  console.log('Checking url: ' + url)
+  const response = await fetch(url)
+  return response.status === 200
+}
+
+async function checkLinks(urls: Set<string>, invalidLinks: string[]) {
+  for (const url of urls) {
+    try {
+        const linkIsValid = await checkLink(url)
+
+        if (!linkIsValid)
+            invalidLinks.push(url)
+
+    } catch (error) {
+        invalidLinks.push(url)
+    }
+  }
 }
 
 main()
